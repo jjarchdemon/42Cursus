bec972c36abd763595f30e23f0a1bbb5fd14717d
Joseph (42Berlin) <jambatt@42berlin.de>
2024-12-04 10:23:35 +0100

    Fixed calloc Changed author name

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -11,12 +11,15 @@
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdint.h>
 #include "libft.h"
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
-	
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 	if (nmemb > SIZE_MAX / size)
 		return (NULL);
 	ptr = malloc(nmemb * size);
