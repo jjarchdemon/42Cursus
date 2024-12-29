e9f33c314b54a545a1cfe1d3bb10e26350706cb6
Joseph(Mac) <jj.archdemon@gmail.com>
2024-12-29 14:13:32 +0100

    Add header file

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -11,7 +11,20 @@
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 120
+# endif
+
+# include <stdlib.h>
+# include <stdint.h>
 
 char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+
 #endif
