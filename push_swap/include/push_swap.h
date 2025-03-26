406ecf387baf491d6a8eac6cef25ee7aa4cdaa3a
Joseph(Mac) <jj.archdemon@gmail.com>
2025-03-26 13:23:38 +0100

    Add header file

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -13,8 +13,35 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-include "../my_libft/ft_printf.h"
-include "../my_libft/get_next_line.h"
-include "../my_libft/libft.h"
+include "../my_libft/ft_printf.h"	//stddef
+include "../my_libft/get_next_line.h"	//stdlib stdint unistd fcntl
+include "../my_libft/libft.h"		//unistd stdarg
+
+include <stdbool.h>			//true & false
+include <limits.h>			//int min & max
+
+typedef struct s_node
+{
+	int	num;
+	int	index;
+	int	push_cost;
+	bool	above_median;
+	bool	cheapest;
+	struct s_node	*next;
+	struct s_node	*prev;
+	struct s_node	*target;
+}	t_node;
+
+//handle errors
+
+//stack initiation
+
+//nodes initiation
+
+//stack utils
+
+//commands
+
+//algo
 
 #endif
