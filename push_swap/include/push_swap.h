fb82c141edb9321fdc4720af9de8940f22a044ba
Joseph(Mac) <jj.archdemon@gmail.com>
2025-03-27 22:26:35 +0100

    Add instruction_swap.c
    Added sa(), sb(), and ss().

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -54,6 +54,10 @@ void    rr(t_node **a, t_node **b);
 void    rra(t_node **a);
 void    rrb(t_node **b);
 void    rrr(t_node **a, t_node **b);
+void    sa(t_node **a);
+void    sb(t_node **b);
+void    ss(t_node **a, t_node **b);
+
 //algo
 
 #endif
--- push_swap/src/instruction_swap.c
+++ push_swap/src/instruction_swap.c
@@ -1,2 +1,39 @@
 /* ************************************************************************** */
-swap
+
+#include "../include/push_swap.h"
+
+static void	swap(t_node **stack)
+{
+	t_node	*temp;
+
+	if (!*stack || !(*stack)->next)
+		return ;
+	temp = (*stack)->next;
+	(*stack)->next = temp->next;
+	if (temp->next)
+		temp->next->prev = *stack;
+
+	temp->prev = NULL;
+	temp->next = *stack;
+	(*stack)->prev = temp;
+	*stack = temp;
+}
+
+void	sa(t_node **a)
+{
+	swap(a);
+	ft_printf("sa\n");
+}
+
+void	sb(t_node **b)
+{
+	swap(b);
+	ft_printf("sb\n");
+}
+
+void	ss(t_node **a, t_node **b)
+{
+	swap(a);
+	swap(b);
+	ft_printf("ss\n");
+}
