e15f3aa7fe156286826049023e53c7b50b4e30b4
Joseph(Mac) <jj.archdemon@gmail.com>
2025-04-02 15:15:31 +0200

    Add push_to_top()
    Added fn to push the selected node to the top of stack

--- push_swap/src/utils_stack.c
+++ push_swap/src/utils_stack.c
@@ -29,3 +29,24 @@ bool	is_sorted(t_node *stack)
 	}
 	return (true);
 }
+
+void push_to_top(t_node **stack, t_node *top_node, char stack_name)
+{
+	while (*stack != top_node)
+	{
+		if (stack_name == 'a')
+		{
+			if (top_node->above_median)
+				ra(stack);
+			else
+				rra(stack);
+		}
+		else if (stack_name == 'b')
+		{
+			if (top_node->above_median)
+				rb(stack);
+			else
+				rrb(stack);
+		}
+	}
+}
