07aa883e1c1ea5e4925e271cb25dfbb5ea60faaf
Joseph (42Berlin) <jambatt@42berlin.de>
2025-04-07 14:51:55 +0200

    Fix some valgrind leaks
    No leaks when arguments are correct. Still leaks present when
    arguments are wrong.

--- push_swap/src/main.c
+++ push_swap/src/main.c
@@ -6,11 +6,38 @@
 /*   By: jambatt <jambatt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 17:09:11 by jambatt           #+#    #+#             */
-/*   Updated: 2025/04/07 13:02:04 by jambatt          ###   ########.fr       */
+/*   Updated: 2025/04/07 14:41:13 by jambatt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
+
+static void	free_split(char **split)
+{
+	int	i;
+
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
+
+static void	handle_input(int ac, char **av, t_node **a)
+{
+    char	**temp;
+
+    if (ac == 2)
+    {
+        temp = ft_split(av[1], ' ');
+        init_stack_a(a, temp);
+        free_split(temp);
+    }
+    else
+        init_stack_a(a, av + 1);
+}
 
 int	main(int ac, char **av)
 {
@@ -21,13 +48,7 @@ int	main(int ac, char **av)
 	b = NULL;
 	if (ac == 1 || (ac == 2 && !av[1][0]))
 		return (1);
-	else if (ac == 2)
-	{
-		av = split(av[1]);
-		init_stack_a(&a, av);
-	}
-	else
-		init_stack_a(&a, av + 1);
+	handle_input(ac, av, &a);
 	if (!is_sorted(a))
 	{
 		if (stack_len(a) == 2)
@@ -38,5 +59,6 @@ int	main(int ac, char **av)
 			sort_turk(&a, &b);
 	}
 	free_stack(&a);
+	free_stack(&b);
 	return (0);
 }
