2ea4356f25255703d7d7c057196413f7f9b228df
Joseph (42Berlin) <jambatt@42berlin.de>
2025-04-10 15:25:13 +0200

    Create utils_more.c
    Put all the other utils in here for now. Need to restructure code
    better.

--- FdF/src/read_map_data.c
+++ FdF/src/read_map_data.c
@@ -6,7 +6,7 @@
 /*   By: jambatt <jambatt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 10:35:10 by jambatt           #+#    #+#             */
-/*   Updated: 2025/04/10 15:12:20 by jambatt          ###   ########.fr       */
+/*   Updated: 2025/04/10 15:23:27 by jambatt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,34 +81,6 @@ static int	fill_map_data(t_fdf *data, int fd)
 	return (0);
 }
 
-//width = no of integers in a line i.e. columns
-//height = no of lines i.e. rows
-//determines the dimensions of the data_map by reading the file line by line
-int	get_size(char *file, int *height, int *width)
-{
-	int		i;
-	int		fd;
-	char	*line;
-
-	i = 0;
-	line = NULL;
-	fd = open(file, O_RDONLY);
-	if (fd == -1)
-		return (-1);
-	line = get_next_line(fd);
-	*width = count_words(line, ' ');
-	while (line)
-	{
-		free(line);
-		line = get_next_line(fd);
-		i++;
-	}
-	free(line);
-	*height = i;
-	close(fd);
-	return (i);
-}
-
 int	*parse_map_line(int **row, int column_count, char *line_content)
 {
 	int		i;
@@ -130,48 +102,4 @@ int	*parse_map_line(int **row, int column_count, char *line_content)
 	}
 	free_arr(number);
 	return (*row);
-}
-
-int	find_max_z(t_fdf *data)
-{
-	int	max_z;
-	int	i;
-	int	j;
-
-	max_z = data->data_map.input_map[0][0][0]; 
-	i = 0;
-	while (i < data->data_map.height)
-	{
-		j = 0;
-		while (j < data->data_map.width)
-		{
-			if (data->data_map.input_map[i][j][0] > max_z)
-				max_z = data->data_map.input_map[i][j][0];
-			j++;
-		}
-		i++;
-	}
-	return (max_z);
-}
-
-int	find_min_z(t_fdf *data)
-{
-	int	min_z;
-	int	i;
-	int	j;
-
-	min_z = data->data_map.input_map[0][0][0]; 
-	i = 0;
-	while (i < data->data_map.height)
-	{
-		j = 0;
-		while (j < data->data_map.width)
-		{
-			if (data->data_map.input_map[i][j][0] < min_z)
-				min_z = data->data_map.input_map[i][j][0];
-			j++;
-		}
-		i++;
-	}
-	return (min_z);
 }
