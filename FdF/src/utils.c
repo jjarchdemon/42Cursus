381caa61a93168e3280f0701d493de354e793482
Joseph (42Berlin) <jambatt@42berlin.de>
2025-04-10 14:11:57 +0200

    Create utils_mem.c
    Put all the memory related utils in utils_mem.c

--- FdF/src/utils.c
+++ FdF/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: jambatt <jambatt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 10:35:22 by jambatt           #+#    #+#             */
-/*   Updated: 2025/04/10 14:01:56 by jambatt          ###   ########.fr       */
+/*   Updated: 2025/04/10 14:10:52 by jambatt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,66 +26,6 @@ t_fdf	*create_fdf_data(void)
 	data->address_data = NULL;
 	data->data_map.input_map = NULL;
 	return (data);
-}
-
-int	clean_close(t_fdf *data)
-{
-	if (data)
-	{
-		if (data->mlx)
-		{
-			if (data->wnd)
-				mlx_destroy_window(data->mlx, data->wnd);
-			if (data->img)
-				mlx_destroy_image(data->mlx, data->img);
-			free(data->mlx);
-		}
-		free_map(data->data_map.input_map, data->data_map.height,
-			data->data_map.width);
-		free(data);
-	}
-	exit(0);
-	return (0);
-}
-
-void	free_arr(char **str)
-{
-	int	i;
-
-	i = 0;
-	if (!str)
-		return ;
-	while (str[i])
-	{
-		free(str[i]);
-		i++;
-	}
-	free(str);
-}
-
-void	free_map(int ***arr, int height, int width)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	if (!arr)
-		return ;
-	while (i < height)
-	{
-		if (arr[i])
-		{
-			j = 0;
-			while (j < width)
-			{
-				free(arr[i][j]);
-				j++;
-			}
-			free(arr[i]);
-		}
-		i++;
-	}
-	free(arr);
 }
 
 static int	get_hex_value(char c)
