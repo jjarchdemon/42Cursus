875e9683f8faa453efe6510da1f47ba79b1591ec
Joseph (42Berlin) <jambatt@42berlin.de>
2025-04-10 15:16:33 +0200

    Split read_map_data() into smaller fns
    As the title says

--- FdF/src/read_map_data.c
+++ FdF/src/read_map_data.c
@@ -6,48 +6,66 @@
 /*   By: jambatt <jambatt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 10:35:10 by jambatt           #+#    #+#             */
-/*   Updated: 2025/04/10 14:03:51 by jambatt          ###   ########.fr       */
+/*   Updated: 2025/04/10 15:12:20 by jambatt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fdf.h"
 
+static int	allocate_map_memory(t_fdf *data);
+static int	fill_map_data(t_fdf *data, int fd);
+
 t_fdf	*read_map_data(t_fdf *data, char *file)
 {
-	int		fd;
-	int		i;
-	char	*line;
+	int	fd;
 
-	i = 0;
 	fd = open(file, O_RDONLY);
 	if (fd == -1)
 		return (free(data), NULL);
 	if (get_size(file, &data->data_map.height, &data->data_map.width) == 1)
 		return (free(data), NULL);
+	if (allocate_map_memory(data) == -1)
+		return (NULL);
+	if (fill_map_data(data, fd) == -1)
+		return (NULL);
+	data->data_map.max_z = find_max_z(data);
+	data->data_map.min_z = find_min_z(data);
+	close(fd);
+	return (data);
+}
+
+static int	allocate_map_memory(t_fdf *data)
+{
+	int	i;
+	int	j;
+
 	data->data_map.input_map = malloc(sizeof(int **) * (data->data_map.height + 1));
 	if (!data->data_map.input_map)
-		return (free(data), NULL);
+		return (free(data), -1);
+	i = 0;
 	while (i < data->data_map.height)
 	{
 		data->data_map.input_map[i] = malloc(sizeof(int *) * data->data_map.width);
 		if (!data->data_map.input_map[i])
-		{
-			free_map(data->data_map.input_map, i, data->data_map.width);
-			free(data);
-			return (NULL);
-		}
-		for (int j = 0; j < data->data_map.width; j++)
+			return (free_map(data->data_map.input_map, i, data->data_map.width), free(data), -1);
+		j = 0;
+		while (j < data->data_map.width)
 		{
 			data->data_map.input_map[i][j] = malloc(sizeof(int) * 2);
 			if (!data->data_map.input_map[i][j])
-			{
-				free_map(data->data_map.input_map, i, data->data_map.width);
-				free(data);
-				return (NULL);
-			}
+				return (free_map(data->data_map.input_map, i, data->data_map.width), free(data), -1);
+			j++;
 		}
 		i++;
 	}
+	return (0);
+}
+
+static int	fill_map_data(t_fdf *data, int fd)
+{
+	int		i;
+	char	*line;
+
 	i = 0;
 	line = get_next_line(fd);
 	while (i < data->data_map.height && line)
@@ -58,10 +76,9 @@ t_fdf	*read_map_data(t_fdf *data, char *file)
 		i++;
 	}
 	data->data_map.input_map[data->data_map.height] = NULL;
-	data->data_map.max_z = find_max_z(data);
-	data->data_map.min_z = find_min_z(data);
-	close(fd);
-	return (data);
+	if (line)
+		free(line);
+	return (0);
 }
 
 //width = no of integers in a line i.e. columns
