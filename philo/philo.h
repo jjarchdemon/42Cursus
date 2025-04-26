6a4fa3e01e4929f68151ce5e88f15c2354ec1122
Ubuntu WSL <whocares@gmail.com>
2025-04-26 19:28:20 +0000

    Initial commit for philosophers
    Made templates for makefile and philo.h

---
+++ philo/philo.h
@@ -0,0 +1,17 @@
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <pthread.h>   //all pthread functions
+# include <sys/time.h>  //gettimeofday
+# include <stdlib.h>    //malloc, free
+# include <stdio.h>     //printf
+# include <unistd.h>    //write, usleep
+# include <string.h>    //memset
+
+
+
+
+
+
+
+#endif
