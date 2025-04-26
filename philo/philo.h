6abd966c8b702c88a1953c871b53fe847a2ef1f7
Ubuntu WSL <whocares@gmail.com>
2025-04-26 20:24:21 +0000

    Update header
    Began s_philo struct, still defining

--- philo/philo.h
+++ philo/philo.h
@@ -8,10 +8,17 @@
 # include <unistd.h>    //write, usleep
 # include <string.h>    //memset
 
+# define MAX_PHILO 200  //max number of philosophers
 
-
-
-
-
+typedef struct s_philo
+{
+	int			id;				// philosopher id
+	pthread_t	thread;			// thread for philosopher
+	size_t		num_of_philos;	// number of philosophers
+	size_t		time_to_die;	// time to die
+	size_t		time_to_eat;	// time to eat
+	size_t		time_to_sleep;	// time to sleep
+	size_t		num_of_eats;		// number of times to eat
+}	t_philo;
 
 #endif
