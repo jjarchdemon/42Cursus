0832d10d1cf7750947711c060fab48c3511ac554
Joseph(Mac) <jj.archdemon@gmail.com>
2025-04-29 22:04:43 +0200

    Add monitor.c
    Implemented print_message()

---
+++ philo/monitor.c
@@ -0,0 +1,13 @@
+#include "philo.h"
+
+//print messsage
+void print_message(char *message, t_philo *philo, int id)
+{
+    size_t time;
+
+    pthread_mutex_lock(philo->write_lock);
+    time = get_now_time() - philo->start_time;
+    //some condition to check
+        printf("%zu %d %s\n", time, id, message);
+    pthread_mutex_unlock(philo->write_lock);
+}
