4f7d9216b15168bb96596e368a56021c354d6d51
Joseph(Mac) <jj.archdemon@gmail.com>
2025-10-31 15:35:01 +0100

    Make a contact class

--- cpp00/ex01/contact.cpp
+++ cpp00/ex01/contact.cpp
@@ -1,4 +1,35 @@
+#include <iostream>
+#include <string>
 
 class Contact {
 
+private:
+	std::string first_name;
+	std::string last_name;
+	std::string nick_name;
+	std::string phone_number; //int?
+	std::string secret;
+
+public:
+	void addContact() {
+		std::cout << "Enter first name: ";
+		getline(std::cin, first_name);
+		std::cout << "Enter last name: ";
+		getline(std::cin, last_name);
+		std::cout << "Enter nick name: ";
+		getline(std::cin, nick_name);
+		std::cout << "Enter phonenumber: ";
+		getline(std::cin, phone_number);
+		std::cout << "Enter your darkest secret: ";
+		getline(std::cin, secret);
+	}
+
+	void displayDetails() {
+		std::cout << "First name: " << first_name << std::endl;
+		std::cout << "Last name: " << last_name << std::endl;
+		std::cout << "Nick name: " << nick_name << std::endl;
+		std::cout << "Phone number: " << phone_number << std::endl;
+		std::cout << "First name: " << secret << std::endl;
+
+	}
 };
--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -1,1 +1,55 @@
-//start
+#include <iostream>
+#include <string>
+
+class Contact {
+
+private:
+	std::string first_name;
+	std::string last_name;
+	std::string nick_name;
+	std::string phone_number; //int?
+	std::string secret;
+
+public:
+	void makeTemplateContact(){
+		first_name = "Joseph";
+		last_name = "Johnson";
+		nick_name = "joe";
+		phone_number = "1234567";
+		secret = "nice try";
+	}
+
+	void askContact() {
+		std::cout << "Enter first name: ";
+		getline(std::cin, first_name);
+		std::cout << "Enter last name: ";
+		getline(std::cin, last_name);
+		std::cout << "Enter nick name: ";
+		getline(std::cin, nick_name);
+		std::cout << "Enter phonenumber: ";
+		getline(std::cin, phone_number);
+		std::cout << "Enter your darkest secret: ";
+		getline(std::cin, secret);
+	}
+
+	void displayDetails() {
+		std::cout << "Showing contact... " << std::endl;
+
+		std::cout << "First name: " << first_name << std::endl;
+		std::cout << "Last name: " << last_name << std::endl;
+		std::cout << "Nick name: " << nick_name << std::endl;
+		std::cout << "Phone number: " << phone_number << std::endl;
+		std::cout << "Darkest secret: " << secret << std::endl;
+	}
+};
+
+int main()
+{
+	Contact contact;
+	//contact.makeTemplateContact();
+	contact.askContact();
+	contact.displayDetails();
+
+	return 0;
+
+}
