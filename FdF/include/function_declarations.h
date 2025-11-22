c8351ae77e031ea3d7334365b7518c2bf390af52
Joseph(Mac) <jj.archdemon@gmail.com>
2025-11-22 12:10:25 +0100

    feat(deinit): add deinit fns

--- FdF/include/function_declarations.h
+++ FdF/include/function_declarations.h
@@ -6,10 +6,19 @@
 /*   By: jambatt <jambatt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 16:07:42 by jambatt           #+#    #+#             */
-/*   Updated: 2025/11/22 11:46:53 by jambatt          ###   ########.fr       */
+/*   Updated: 2025/11/22 12:08:46 by jambatt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* ------------------ INIT UTILS ----------------------*/
 
 void	init_fdf(t_fdf *fdf);
+
+/* ------------------ DEINIT UTILS ----------------------*/
+
+void    deinit_mlx(t_fdf *fdf);
+void    deinit_img(t_fdf *fdf);
+void    deinit_map(t_fdf *fdf);
+void    deinit_fdf(t_fdf *fdf);
+
+/* ------------------ DEINIT UTILS ----------------------*/
---
+++ FdF/utils/utils_deinit.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_deinit.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jambatt <jambatt@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/22 12:02:00 by jambatt           #+#    #+#             */
+/*   Updated: 2025/11/22 12:07:43 by jambatt          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/fdf.h"
+
+void    deinit_mlx(t_fdf *fdf)
+{
+	if (!fdf)
+		return;
+	if (fdf->win_ptr)
+		mlx_destroy_window(fdf->mlx_ptr, fdf->win_ptr);
+	
+	#ifdef __linux__
+	if (fdf->mlx_ptr)
+	{
+		mlx_destroy_display(fdf->mlx_ptr);
+		free(fdf->mlx_ptr);
+	}
+	#elif __APPLE__
+		fdf->mlx_ptr = NULL;//not necessary?
+	#endif
+}
+
+void    deinit_img(t_fdf *fdf)
+{
+	if (!fdf)
+		return;
+	if (fdf->img)
+	{
+		if (fdf->img->img_ptr)
+			mlx_destroy_image(fdf->mlx_ptr, fdf->img->img_ptr);
+		free(fdf->img);
+	}
+}
+void    deinit_map(t_fdf *fdf)
+{
+	if (!fdf)
+		return;
+	if (fdf->z_map)
+		free(fdf->z_map);
+}
+
+void    deinit_fdf(t_fdf *fdf)
+{
+	deinit_mlx(fdf);
+	deinit_img(fdf);
+	deinit_map(fdf);
+	//put all the values to 0?
+	free(fdf);
+}
