188ed2b830ae0acd8b1e8af6350a567d44d5b7a4
Joseph(Mac) <jj.archdemon@gmail.com>
2025-12-23 11:42:59 +0100

    feat(systems): make mlx fn for mac/linux
    - make separate deinit_mlx() for mac/linux
    - add comments for leftovers in utils_init.c

--- FdF/utils/utils_deinit.c
+++ FdF/utils/utils_deinit.c
@@ -6,11 +6,14 @@
 /*   By: jambatt <jambatt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 12:02:00 by jambatt           #+#    #+#             */
-/*   Updated: 2025/11/22 12:58:16 by jambatt          ###   ########.fr       */
+/*   Updated: 2025/12/23 11:39:32 by jambatt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fdf.h"
+
+// deinit for mlx pointers
+#ifdef __linux__
 
 void    deinit_mlx(t_fdf *fdf)
 {
@@ -19,16 +22,24 @@ void    deinit_mlx(t_fdf *fdf)
 	if (fdf->win_ptr)
 		mlx_destroy_window(fdf->mlx_ptr, fdf->win_ptr);
 	
-	#ifdef __linux__
 	if (fdf->mlx_ptr)
 	{
 		mlx_destroy_display(fdf->mlx_ptr);
 		free(fdf->mlx_ptr);
 	}
-	#elif __APPLE__
-		fdf->mlx_ptr = NULL;//not necessary?
-	#endif
 }
+
+#elif __APPLE__
+
+void    deinit_mlx(t_fdf *fdf)
+{
+	if (!fdf)
+		return;
+	if (fdf->win_ptr)
+		mlx_destroy_window(fdf->mlx_ptr, fdf->win_ptr);
+	fdf->mlx_ptr = NULL;
+}
+#endif
 
 void    deinit_img(t_fdf *fdf)
 {
@@ -54,6 +65,5 @@ void    deinit_fdf(t_fdf *fdf)
 	deinit_img(fdf);
 	deinit_map(fdf);
 	deinit_mlx(fdf);
-	//put all the values to 0?
 	free(fdf);
 }
--- FdF/utils/utils_init.c
+++ FdF/utils/utils_init.c
@@ -6,7 +6,7 @@
 /*   By: jambatt <jambatt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 11:38:55 by jambatt           #+#    #+#             */
-/*   Updated: 2025/11/22 13:15:54 by jambatt          ###   ########.fr       */
+/*   Updated: 2025/12/23 11:29:50 by jambatt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,27 +23,22 @@ void	init_fdf(t_fdf *fdf)
 	fdf->height = 0;
 	fdf->width = 0;
 	init_image(fdf);
-	fdf->zoom = ZOOM_LEVEL;
-	fdf->x_offset = WIDTH / 2;	//why?
-	fdf->y_offset = HEIGHT / 2;	//why?
-	fdf->angle = INITIAL_ANGLE;
+	fdf->zoom = ZOOM_LEVEL;     //TODO: understand
+	fdf->x_offset = WIDTH / 2;  //TODO: understand
+	fdf->y_offset = HEIGHT / 2; //TODO: understand
+	fdf->angle = INITIAL_ANGLE; //TODO: understand
 }
 
 void    init_image(t_fdf *fdf)
 {
     if (!fdf)
         return;
-    //Allocate the struct
     fdf->img = (t_img *)malloc(sizeof(t_img));
     if (!fdf->img)
-        exit(1); // Proper exit
-    
-    //Setup the MLX image directly into new struct
+        exit(1); //TODO: error handling
     fdf->img->img_ptr = mlx_new_image(fdf->mlx_ptr, WIDTH, HEIGHT);
     if (!fdf->img->img_ptr)
-        exit(1);
-
-    //Get the data address directly
+        exit(1);    //TODO: error handling
     fdf->img->data_addr = mlx_get_data_addr(fdf->img->img_ptr, 
         &fdf->img->bits_per_pixel, 
         &fdf->img->size_line, 
